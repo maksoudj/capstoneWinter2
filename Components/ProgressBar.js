807779ecaddd45e9ad314ea56bbb243a203c4b08
Isam Makkie <makkieie@vcu.edu>
2023-02-04 18:52:28 -0500

    Isam's Progress Bar Push

---
+++ Components/ProgressBar.js
@@ -0,0 +1,48 @@
+import React from "react";
+import DataContext from "../Context/FormContext";
+import { useContext } from "react";
+import { color } from "@mui/system";
+
+
+export default function ProgressBar() {
+  
+  const {gradeStandards, matching} = useContext(DataContext);
+  
+  console.log(Object.keys(matching).length)
+  console.log(gradeStandards.length);
+
+  const Progress = Math.round((Object.keys(matching).length / gradeStandards.length) * 100).toString().concat("%");
+
+  console.log(Progress);
+  
+  let progressStatus = "";
+  if(Progress == '100%'){
+    progressStatus = "Matching Completed!"
+  }
+  else{
+    progressStatus = "Matching in progress"
+  }
+
+  return (
+    <div className="relative pt-2 w-[50vw]">
+      <div className="flex mb-2 items-center justify-between">
+        <div>
+          <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-teal-600 bg-teal-200">
+            {progressStatus}
+          </span>
+        </div>
+        <div className="text-right">
+          <span className="text-xs font-semibold inline-block text-teal-600">
+           {Progress}
+          </span>
+        </div>
+      </div>
+      <div className="overflow-hidden h-2 mb-4 text-xs flex rounded bg-teal-200">
+        <div
+          style={{ width: Progress }}
+          className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-teal-500"
+        ></div>
+      </div>
+    </div>
+  );
+}
